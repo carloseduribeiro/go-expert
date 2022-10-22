30b6e2bbf25bfd20d20653c4b103596c4441b827
carloseduribeiro <carlos.eribeiro@br.experian.com>
2022-10-21 22:18:04 -0300

    http-request-timout

---
+++ important-packages/http-request-timeout/main.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"fmt"
+	"io"
+	"net/http"
+	"time"
+)
+
+func main() {
+	c := http.Client{Timeout: time.Microsecond}
+	resp, err := c.Get("https://www.google.com")
+	if err != nil {
+		panic(err)
+	}
+	defer resp.Body.Close()
+	body, err := io.ReadAll(resp.Body)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println(string(body))
+}
