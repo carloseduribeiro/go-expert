fc9eec2b62415740ec7e5182f38b7a136be7d114
carloseduribeiro <carloseribeiro96@gmail.com>
2022-10-25 23:29:41 -0300

    database - inserting data

--- database/1/go.mod
+++ database/1/go.mod
@@ -2,4 +2,7 @@ module github.com/carloseduribeiro/go-expert/database/1
 
 go 1.19
 
-require github.com/google/uuid v1.3.0 // indirect
+require (
+	github.com/go-sql-driver/mysql v1.6.0 // indirect
+	github.com/google/uuid v1.3.0 // indirect
+)
--- database/1/main.go
+++ database/1/main.go
@@ -1,20 +1,48 @@
 package main
 
-import "github.com/google/uuid"
+import (
+	"database/sql"
+	_ "github.com/go-sql-driver/mysql"
+	"github.com/google/uuid"
+)
 
 type Product struct {
 	ID    string
-	name  string
-	price float64
+	Name  string
+	Price float64
 }
 
 func NewProduct(name string, price float64) *Product {
 	return &Product{
 		ID:    uuid.New().String(),
-		name:  name,
-		price: price,
+		Name:  name,
+		Price: price,
 	}
 }
 
 func main() {
+	// user:password@host/database
+	db, err := sql.Open("mysql", "root:root@tcp(localhost:3306)/goexpert")
+	if err != nil {
+		panic(err)
+	}
+	defer db.Close()
+	product := NewProduct("Notebook", 1899.99)
+	err = insertProduct(db, product)
+	if err != nil {
+		panic(err)
+	}
+}
+
+func insertProduct(db *sql.DB, product *Product) error {
+	stmt, err := db.Prepare("INSERT INTO products(id, name, price) VALUES(?, ?, ?)")
+	if err != nil {
+		return err
+	}
+	defer stmt.Close()
+	_, err = stmt.Exec(product.ID, product.Name, product.Price)
+	if err != nil {
+		return err
+	}
+	return nil
 }
