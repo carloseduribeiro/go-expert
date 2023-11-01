fb5fbb46210f756cb0e9049ca5a4149fcac54382
carloseduribeiro <carloseribeiro96@gmail.com>
2023-10-31 22:11:38 -0300

    multithreading: goroutines, wgs, mutext and atomic operations

---
+++ multithreading/02-wait-groups/main.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"fmt"
+	"sync"
+	"time"
+)
+
+func task(name string, wg *sync.WaitGroup) {
+	for i := 0; i < 10; i++ {
+		fmt.Printf("%d: Task %s is running\n", i, name)
+		time.Sleep(1 * time.Second)
+		wg.Done()
+	}
+}
+
+func main() {
+	wg := &sync.WaitGroup{}
+	wg.Add(25)
+	go task("A", wg)
+	go task("B", wg)
+	go func() {
+		for i := 0; i < 5; i++ {
+			fmt.Printf("%d: Task %s is running\n", i, "anonymous")
+			time.Sleep(1 * time.Second)
+			wg.Done()
+		}
+	}()
+	wg.Wait()
+}
